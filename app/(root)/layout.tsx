c8b32720ec14cb74ba011c516604a27e9c3b5fbc
Kim Kudsk <kim.kudsk@gmail.com>
2025-02-16 01:03:54 +0100

    CSS and header

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,9 +1,10 @@
+import Header from '@/components/header';
 import React, { ReactNode } from 'react'
 
 const Layout = ({ children }: { children: ReactNode }) => {
   return <main className="root-container">
     <div className="mx-auto max-x-7xl">
-      Header
+      <Header />
       <div className='mt-20 pb-20'>
         {children}
       </div>
