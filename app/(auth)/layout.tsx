ff243ecc4f84dcbebeccfafc99951a8e618c5a92
Kim Kudsk <kim.kudsk@gmail.com>
2025-02-22 12:21:52 +0100

    auth layout

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,7 +1,29 @@
-import React from "react";
+import Image from "next/image";
+import { ReactNode } from "react";
 
-const Layout = () => {
-  return <div>Layout</div>;
+const Layout = ({ children }: { children: ReactNode }) => {
+  return (
+    <main className="auth-container">
+      <section className="auth-form">
+        <div className="auth-box">
+          <div className="flex flex-row gap-3">
+            <Image src="/icons/logo.svg" alt="logo" width={37} height={37} />
+            <p className="text-2xl font-semibold text-white">BookWise</p>
+          </div>
+          <div>{children}</div>
+        </div>
+      </section>
+      <section className="auth-illustration">
+        <Image
+          src="/images/auth-illustration.png"
+          alt="Auth Illustration"
+          width={1000}
+          height={1000}
+          className="size-full object-cover"
+        />
+      </section>
+    </main>
+  );
 };
 
 export default Layout;
--- components/BookCard.tsx
+++ components/BookCard.tsx
@@ -2,6 +2,8 @@ import Link from "next/link";
 import React from "react";
 import BookCover from "@/components/BookCover";
 import { cn } from "@/lib/utils";
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
 
 const BookCard = ({
   id,
@@ -21,6 +23,22 @@ const BookCard = ({
         <p className="book-title">{title}</p>
         <p className="book-genre">{genre}</p>
       </div>
+      {isLoanedBook && (
+        <div className="mt-3 w-full">
+          <div className="book-loaned">
+            <Image
+              src="/icons/calendar.svg"
+              alt="calendar"
+              width={18}
+              height={18}
+              className="object-contain"
+            />
+            <p className="text-light-100">11 days left to return</p>
+          </div>
+
+          <Button className="book-btn">Download receipt</Button>
+        </div>
+      )}
     </Link>
   </li>
 );
